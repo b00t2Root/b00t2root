714b645aea9b2769b7e65dc1afde4fff11d553de
AdityaVallabh <adityavallabhganti@gmail.com>
2017-12-12 17:46:15 +0530

    Fix faq toggle

--- js/faq.js
+++ js/faq.js
@@ -4,20 +4,19 @@ var i,k;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
+        var panel = this.nextElementSibling;
         this.classList.toggle("active");
         for(k = 0; k < acc.length ; k++) {
           acc[k].classList.toggle("active");
           var panel_temp = acc[k].nextElementSibling;
-          panel_temp.style.display = "none";
+          if(panel != panel_temp)
+            panel_temp.style.display = "none";
         }
-
-        var panel = this.nextElementSibling;
-
+        
         if (panel.style.display === "block") {
             panel.style.display = "none";
         } else {
             panel.style.display = "block";
-            console.log("adnda")
         }
     }
 }
